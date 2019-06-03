23899926eb7921a4dcddeab6dfe19b50f13a638f
Oscar FM <ofmz@autistici.org>
2019-06-03 19:22:02 +0100

    Fun is ... to code FunColorSwitchFromOrange2Blue

--- js/code.js
+++ js/code.js
@@ -1,9 +1,25 @@
-'use strict'
+'use strict';
 
 // eslint-disable-next-line camelcase
 const user_profile = {
-  'intro': '<b>A B C D F G</b> <br><br> Introduction to Web Technologies,<br> sharing them while I learn them and use them here!!',
-  'player1': '<iframe style="border: 0; width: 50%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/album=997256964/size=small/bgcol=333333/linkcol=0f91ff/track=398268745/transparent=true/" seamless><a href="http://odicforcesounds.bandcamp.com/album/spiritual-algorithm">Spiritual Algorithm by Odicforcesounds</a></iframe>',
+  'intro': [
+    {
+      'TopMessage': '<b>A B C D F G</b> <br><br> Introduction to Web Technologies,<br> sharing them while I learn them and use them here!!'
+    },
+    {
+      'TopMessage': '<b>A B C D F G</b> <br><br> Introduction to Functions!'
+    }
+  ],
+  'player': [
+    {
+      'title': 'Dive Into the World of JavaScrpt',
+      'track': '<iframe style="border: 0; width: 50%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/album=997256964/size=small/bgcol=333333/linkcol=0f91ff/track=398268745/transparent=true/" seamless><a href="http://odicforcesounds.bandcamp.com/album/spiritual-algorithm">Spiritual Algorithm by Odicforcesounds</a></iframe>'
+    },
+    {
+      'title': 'Data - Objects and Functions',
+      'track': '<iframe style="border: 0; width: 50%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/album=997256964/size=small/bgcol=333333/linkcol=0f91ff/track=4209853396/transparent=true/" seamless><a href="http://odicforcesounds.bandcamp.com/album/spiritual-algorithm">Spiritual Algorithm by Odicforcesounds</a></iframe>'
+    }
+  ],
   'pnote': 'So here I am Diving Into the Code I should to finish my Quest!',
   'postsList': [
     {
@@ -24,39 +40,44 @@ const user_profile = {
       'link': 'https://github.com/rakzhodekams'
     }
   ]
-}
+};
 
 // LOGO
-const logo = document.getElementById('logo')
+const logo = document.getElementById('logo');
 logo.innerHTML = user_profile.logo
 
 // INTRO
-const intro = document.getElementById('mintro')
+const intro = document.getElementById('mintro');
 intro.innerHTML = user_profile.intro
-const note = document.getElementById('pnote')
+const note = document.getElementById('pnote');
 note.innerHTML = user_profile.pnote
 
 // BANDCAMP TRACK
-const player = document.getElementById('player1')
-player.innerHTML = user_profile.player1
+const player = document.getElementById('player1');
+player.innerHTML = user_profile.player[0].track
 
 // INSERT FIRST POST DATA
-const firstPost = document.getElementById('pcode1')
+const firstPost = document.getElementById('pcode1');
 firstPost.innerHTML = user_profile.postsList[0].content
 
 // document.getElementById('pcode1').innerHTML = user_profile.pcode1
 
 // NEXT PAGE - UNDERSTANDING FUNCTIONS
-const button = document.getElementById('btn')
-button.addEventListener('click', nextPage )
+const button = document.getElementById('btn');
+button.addEventListener('click', nextPage );
 function nextPage(e) {
   let i = 0;
-  while( i < user_profile.postsList[i].content.length) {
-    let post = document.getElementById('pcode1')
+  let j = 0;
+  while( i < user_profile.postsList[i].content.length, i < user_profile.player[j].track.length) {
+    let player = document.getElementById('player1');
+    let post = document.getElementById('pcode1');
+    player.innerHTML = user_profile.player[j].track
     post.innerHTML = user_profile.postsList[i].content
-    i++    
+    j++;
+    i++;
   }
 }
+
 // GITHUB FOOT
 const gh = document.getElementById('github')
 gh.innerHTML = '<a href=' + user_profile.open_source[0].link + ' ' + 'target=_blank>GitHub</a>'
--- js/functions.js
+++ js/functions.js
@@ -2,10 +2,10 @@
 
 // eslint-disable-next-line camelcase
 var user_profile = {
-  'intro': '<b>A B C D F G</b> <br><br> JavaScript Functions',
-  'player1': '<iframe style="border: 0; width: 50%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/album=997256964/size=small/bgcol=333333/linkcol=0f91ff/track=4209853396/transparent=true/" seamless><a href="http://odicforcesounds.bandcamp.com/album/spiritual-algorithm">Spiritual Algorithm by Odicforcesounds</a></iframe>',
+  'intro': '',
+  'player1': '',
   'pnote': 'So here I am Diving Into the Code I should to finish my Quest!',
-  'pcode1': '<h2> - Q: You know what is Fun?</h2 <br> - A: Yes, Actions! <br> - % Master: So type those <b>codes</b> that exist there ( as you can see them ) and type them with your fingers, and see ( test ) their behaviors, while you learn! ',
+  'pcode1': '<h2> - Q: You know what is Fun?</h2 <br> - A: Yes, Actions! <br><br> % Master: So type those <b>codes</b> with your fingers which are the <b>DOM</b> Objects, the Browser can interpret (without a lot mistakes (compared to humans :P)), and see ( test ) their behaviors, while you learn! ',
   'logo': '<a href="https://odicforcesounds.com/" target="_blank"><img id="logo" src="https://path_of_shields.keybase.pub/Myth-illusions-Memories/SpaceTrip/WeCameInPeace3.jpg" alt=""></a>',
   'open_source': [
     {
@@ -36,11 +36,6 @@ post1.innerHTML = user_profile.pcode1
 // INSERT NEXT POST
 let post2 = document.getElementById('pcode2')
 // post2.innerHTML = user_profile.pcode2
-
-// NEXT PAGE - UNDERSTANDING FUNCTIONS
-
-
-
 
 // GITHUB FOOT
 document.getElementById('github').innerHTML = '<a href=' + user_profile.open_source[0].link + ' ' + 'target=_blank>GitHub</a>'
