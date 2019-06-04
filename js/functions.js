9cdb693439f31077a7d298129aed960d7378d506
Oscar FM <ofmz@autistici.org>
2019-06-04 08:56:33 +0100

    Fixing TopMenuArtLinkColor to green + intro

--- js/functions.js
+++ js/functions.js
@@ -2,11 +2,11 @@
 
 // eslint-disable-next-line camelcase
 var user_profile = {
-  'intro': '',
-  'player1': '',
+  'intro': '<b>A B C D F G</b> <br><br> Introduction to Functions!',
+  'player1': '<iframe style="border: 0; width: 50%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/album=997256964/size=small/bgcol=333333/linkcol=0f91ff/track=4209853396/transparent=true/" seamless><a href="http://odicforcesounds.bandcamp.com/album/spiritual-algorithm">Spiritual Algorithm by Odicforcesounds</a></iframe>',
   'pnote': 'So here I am Diving Into the Code I should to finish my Quest!',
   'pcode1': '<h2> - Q: You know what is Fun?</h2 <br> - A: Yes, Actions! <br><br> % Master: So type those <b>codes</b> with your fingers which are the <b>DOM</b> Objects, the Browser can interpret (without a lot mistakes (compared to humans :P)), and see ( test ) their behaviors, while you learn! ',
-  'logo': '<a href="https://odicforcesounds.com/" target="_blank"><img id="logo" src="https://path_of_shields.keybase.pub/Myth-illusions-Memories/SpaceTrip/WeCameInPeace3.jpg" alt=""></a>',
+  'logo': '<img id="logo" src="https://path_of_shields.keybase.pub/Myth-illusions-Memories/SpaceTrip/WeCameInPeace3.jpg" alt="">',
   'open_source': [
     {
       'description': 'GitHub',
