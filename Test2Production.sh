11478953ff6b09ed9e39e61c17ccb634637d40ca
Oscar FM <ofmz@autistici.org>
2019-06-05 17:28:43 +0100

    simple shell script to copy tested code to Live

--- Test2Production.sh
+++ Test2Production.sh
@@ -6,3 +6,16 @@
 # (/src/) to HTML FILES 
 # (/js/) to JavaScript FILES
 # (/styles/) to CSS FILES
+# I am Using Linux for the last 20 years, so...
+
+cp testing/src/* src/
+
+echo "HTML Files Copied"
+
+cp testing/js/* js/
+
+echo "JS Files Copied"
+
+cp testing/styles/*.css styles/
+
+echo "CSS Files Copied"
