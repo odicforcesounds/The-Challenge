1c98d14df5f350dcd240bc7900c5988a1bf98be4
Oscar FM <ofmz@autistici.org>
2019-06-05 14:50:33 +0100

    Shell Script ( not defined ) to copy tested#passed code to /

---
+++ Test2Production.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# Author: Oscar FM aka SunWukong aka OdicforceSounds
+# This Script will define several little jobs. 
+# The /testing/ folder is where I test before 
+# I public in the / folder of this repository 
+# (/src/) to HTML FILES 
+# (/js/) to JavaScript FILES
+# (/styles/) to CSS FILES
