3378422f5ecb678f327a6c10cd7c16221820b918
Oscar FM <ofmz@autistici.org>
2019-06-05 05:23:51 +0100

    CodeTopMenuPage is a Dynamic WebPage ( better then Angular) .. Joke! Is Working! Stage01End Stage02Start

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,8 @@
 
 // eslint-disable-next-line camelcase
 var user_profile = {
-  'intro': '<b>A B C D F G</b> <br><br> Introduction to Functions!',
+  'subtitle': 'What is Not a Function?',
+  'intro': '<b>A B C D F G</b> <br> Introduction to Functions!',
   'player1': '<iframe style="border: 0; width: 50%; height: 42px;" src="https://bandcamp.com/EmbeddedPlayer/album=997256964/size=small/bgcol=333333/linkcol=0f91ff/track=4209853396/transparent=true/" seamless><a href="http://odicforcesounds.bandcamp.com/album/spiritual-algorithm">Spiritual Algorithm by Odicforcesounds</a></iframe>',
   'pnote': 'So here I am Diving Into the Code I should to finish my Quest!',
   'pcode1': '<h2> - Q: You know what is Fun?</h2 <br> - A: Yes, Actions! <br><br> % Master: So type those <b>codes</b> with your fingers which are the <b>DOM</b> Objects, the Browser can interpret (without a lot mistakes (compared to humans :P)), and see ( test ) their behaviors, while you learn! ',
@@ -19,23 +20,21 @@ var user_profile = {
 let logo = document.getElementById('logo')
 logo.innerHTML = user_profile.logo
 
-// INTRO
-let welcome = document.getElementById('mintro')
-welcome.innerHTML = user_profile.intro
-
-let hereiam = document.getElementById('pnote')
-hereiam.innerHTML = user_profile.pnote
-
 // BANDCAMP TRACK
 let player = document.getElementById('player1')
 player.innerHTML = user_profile.player1
 
+// INTRO
+let fun = document.getElementById('subtitle')
+let into = document.getElementById('mintro')
+let hereiam = document.getElementById('pnote')
+fun.innerHTML = user_profile.subtitle
+into.innerHTML = user_profile.intro
+hereiam.innerHTML = user_profile.pnote
+
 // INSERT FIRST POST DATA
 let post1 = document.getElementById('pcode1')
 post1.innerHTML = user_profile.pcode1
-// INSERT NEXT POST
-let post2 = document.getElementById('pcode2')
-// post2.innerHTML = user_profile.pcode2
 
 // GITHUB FOOT
 document.getElementById('github').innerHTML = '<a href=' + user_profile.open_source[0].link + ' ' + 'target=_blank>GitHub</a>'
