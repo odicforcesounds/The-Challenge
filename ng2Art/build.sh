bfccc364bf362771634c231a7de1eb8754acbb57
Rakzhodekams <ofmz@autistici.org>
2020-04-18 01:21:19 +0100

    deep random meanings

--- ng2Art/build.sh
+++ ng2Art/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng build --prod --aot --build-optimizer --base-href "https://wiki.odicforcesounds.com/#/"
+node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng build --prod --aot --build-optimizer --base-href "https://wiki.odicforcesounds.com/v/"
