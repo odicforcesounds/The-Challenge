f96d938a7355852387511c28254298666ba7f5f2
Rakzhodekams <ofmz@autistici.org>
2022-02-01 01:02:53 +0000

    clean out

--- trash/garbage/ng2Art/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-node --max_old_space_size=8192 ./node_modules/@angular/cli/bin/ng build --configuration production --aot --build-optimizer --base-href "https://wiki.odicforcesounds.com/v/"
